b0c6beeb1a57d249e09c9b7b58606fc42be7b2c3
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-08-29 15:49:27 +0200

    HW16 is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,62 @@
-import "./App.css";
 import { useState } from "react";
-import { Button, Modal } from "antd";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  const showModal = () => {
-    setIsModalOpen(true);
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <Button type="primary" onClick={showModal}>
-        Открыть модальное окно
-      </Button>
-      <Modal
-        title="Простое модальное окно"
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        okText="OK"
-        cancelText="Cancel"
-      >
-        <p>
-          Это простое модальное окно. Вы можете настроить его по своему
-          усмотрению.
-        </p>
-      </Modal>
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React HW16 - Material UI
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 4 }}>
+        <Typography variant="h5" gutterBottom>
+          Добро пожаловать!
+        </Typography>
+        <Button variant="contained" onClick={handleClickOpen}>
+          Открыть диалог
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Простое диалоговое окно</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Это простое диалоговое окно. Вы можете закрыть его, нажав одну из
+              кнопок ниже.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Отмена</Button>
+            <Button onClick={handleClose} autoFocus>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Container>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "antd/dist/reset.css";
 
 createRoot(document.getElementById("root")).render(<App />);
